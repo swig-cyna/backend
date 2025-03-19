4278794d7fbee14b96c3d9c075d4e0a5baa16343
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-03-19 19:38:10 +0100

    fix(product): add stripe default value for seed

--- src/db/seeds/1739527090945_products.ts
+++ src/db/seeds/1739527090945_products.ts
@@ -4,9 +4,27 @@ export async function seed(db: Kysely<any>): Promise<void> {
   await db
     .insertInto("products")
     .values([
-      { name: "Product 1", price: 10.99, description: "Description 1" },
-      { name: "Product 2", price: 19.99, description: "Description 2" },
-      { name: "Product 3", price: 29.99, description: "Description 3" },
+      {
+        name: "Product 1",
+        price: 10.99,
+        description: "Description 1",
+        stripe_product_id: "prod_123",
+        stripe_price_id: "price_123",
+      },
+      {
+        name: "Product 2",
+        price: 19.99,
+        description: "Description 2",
+        stripe_product_id: "prod_456",
+        stripe_price_id: "price_456",
+      },
+      {
+        name: "Product 3",
+        price: 29.99,
+        description: "Description 3",
+        stripe_product_id: "prod_789",
+        stripe_price_id: "price_789",
+      },
     ])
     .execute()
 }
