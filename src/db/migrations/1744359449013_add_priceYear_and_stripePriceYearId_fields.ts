bdb28292c61b51c2104f3061f2912f182012911f
dissemblable <julesvallet23@gmail.com>
2025-04-11 11:06:38 +0200

    fix(product): change handler to match with new fields name

--- src/db/migrations/1744359449013_add_priceYear_and_stripePriceYearId_fields.ts
+++ src/db/migrations/1744359449013_add_priceYear_and_stripePriceYearId_fields.ts
@@ -9,3 +9,13 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn("stripe_price_year_id", "varchar(255)", (col) => col.notNull())
     .execute()
 }
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable("products")
+    .dropColumn("price_month")
+    .dropColumn("price_year")
+    .dropColumn("stripe_price_month_id")
+    .dropColumn("stripe_price_year_id")
+    .execute()
+}
