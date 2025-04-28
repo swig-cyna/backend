d103e42a7b22ba35af5698f76724ad78eba658ba
Ralex91 <95540504+Ralex91@users.noreply.github.com>
2025-04-28 15:01:58 +0200

    feat(passkey): add db migration

---
+++ src/db/migrations/1745844399095_add_passkey_table.ts
@@ -0,0 +1,25 @@
+import { sql, type Kysely } from "kysely"
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable("passkeys")
+    .addColumn("id", "text", (col) => col.primaryKey().notNull())
+    .addColumn("name", "text")
+    .addColumn("publicKey", "text", (col) => col.notNull())
+    .addColumn("userId", "text", (col) =>
+      col.references("user.id").onDelete("cascade").notNull(),
+    )
+    .addColumn("credentialID", "text", (col) => col.notNull())
+    .addColumn("counter", "integer", (col) => col.notNull())
+    .addColumn("deviceType", "text")
+    .addColumn("backedUp", "boolean")
+    .addColumn("transports", "text")
+    .addColumn("createdAt", "timestamp", (col) =>
+      col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`),
+    )
+    .execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable("passkeys").execute()
+}
